8962e45ed2618583794bc14305f03ba27acbd0dc
Qing Shi <shiqing881215@gmail.com>
2016-08-15 21:18:25 -0700

    Update by adding style

--- src/main/java/org/shiqing/ibd/Analyzer.java
+++ src/main/java/org/shiqing/ibd/Analyzer.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -13,6 +11,8 @@ import java.util.Set;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.shiqing.ibd.model.Stock;
 import org.shiqing.ibd.model.StockAnalyzeResult;
@@ -88,6 +88,12 @@ public class Analyzer {
 			symbolCell.setCellValue(stockAnalyzeResult.getSymbol());
 			nameCell.setCellValue(stockAnalyzeResult.getName());
 			occuranceCell.setCellValue(stockAnalyzeResult.getOccurance());
+			if (stockAnalyzeResult.getOccurance() >= 3) {
+				CellStyle style = workbook.createCellStyle();
+				style.setFillBackgroundColor(IndexedColors.RED.getIndex());
+				style.setFillPattern(CellStyle.ALIGN_FILL);
+				occuranceCell.setCellStyle(style);
+			}
 			involvedSpreadsheetsCell.setCellValue(stockAnalyzeResult.getInvolvedSpreadsheets().toString());
 		}
 		
